0e3b6f45dd05fd19791bfce1e17ddcad9670119a
Leandro Esneyder Agudelo V <leandroagudelo@gmail.com>
2019-04-10 10:22:09 -0500

    Crear tablespace

--- expdp.sql
+++ expdp.sql
@@ -2,3 +2,14 @@ Export
 expdp sys/oracle@ORCL directory=BK_PRUEBA schemas=LEO dumpfile=LEO-$(date +%Y%m%d%H%M).dmp logfile=LEO-$(date +%Y%m%d%H%M).log grants=Y
 import
 impdp sys/oracle@ORCL directory=BK_PRUEBA schemas=LEO dumpfile=LEO-$(date +%Y%m%d%H%M).dmp logfile=LEO-$(date +%Y%m%d%H%M).log grants=Y
+
+
+
+CREATE TABLESPACE USERS DATAFILE 
+  '/home/u01/app/oracle/oradata/orcl/pdbdevaliusers.DAT' SIZE 2000M AUTOEXTEND ON NEXT 1024M MAXSIZE UNLIMITED
+LOGGING
+ONLINE
+EXTENT MANAGEMENT LOCAL AUTOALLOCATE
+BLOCKSIZE 8K
+SEGMENT SPACE MANAGEMENT AUTO
+FLASHBACK ON;
