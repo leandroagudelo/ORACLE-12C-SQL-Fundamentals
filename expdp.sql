a7122735cbdab3094a54355647e8d17560db4703
Leandro Esneyder Agudelo V <leandroagudelo@gmail.com>
2019-04-10 09:11:33 -0500

    Update expdp.sql

--- expdp.sql
+++ expdp.sql
@@ -1,4 +1,4 @@
 Export
-expdp sys/oracle@ORCL directory=BK_PRUEBA schemas=LEO dumpfile=LEO-$(date +%Y%m%d%H%M).dmp
+expdp sys/oracle@ORCL directory=BK_PRUEBA schemas=LEO dumpfile=LEO-$(date +%Y%m%d%H%M).dmp logfile=LEO-$(date +%Y%m%d%H%M).log grants=Y
 import
-impdp sys/oracle@ORCL directory=BK_PRUEBA schemas=LEO dumpfile=LEO-$(date +%Y%m%d%H%M).dmp
+impdp sys/oracle@ORCL directory=BK_PRUEBA schemas=LEO dumpfile=LEO-$(date +%Y%m%d%H%M).dmp logfile=LEO-$(date +%Y%m%d%H%M).log grants=Y
